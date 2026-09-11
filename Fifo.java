7fce408043167e2049c3b0ca12433a4eb61886d7
Isabella C Melo <127143733+IsabellaCarvalh0@users.noreply.github.com>
2026-09-11 16:57:57 -0300

    Update Fifo.java

--- Fifo.java
+++ Fifo.java
@@ -1,20 +1,117 @@
 import java.util.*;
+
 public class Fifo {
 
-    private Processo[][] matriz;
+    private int[][] matriz;
+    private int tempoMedioDeEspera;
+    private int tempoMedioDeResposta;
+    private int turnaroundMedio;
 
-    public Fifo(int quantidadeDeProcessos, int tempoDeExecucaoTotal) {
-        matriz = new Processo[quantidadeDeProcessos][tempoDeExecucaoTotal];
+    public Fifo(int quantidadeDeProcessos, int tempoDeExecucaoTotal, List<Processo> processos) {
+        
+        // ------------------------CRIANDO A TABELA----------------------------------
+
+        matriz = new int[quantidadeDeProcessos][tempoDeExecucaoTotal];
+        // Para preencher a matriz:
         // 0 = fazendo nada
         // 1 = executando
         // 2 = esperando
-        for (int j = 0; j< matriz[0].length; j++){
-            for(int i = 0; i < matriz.length; i++){
 
+        /*
+            para cada i, uma linha da matriz:
+
+            linha 1 = processo 1
+            linha 2 = processo 2
+            .
+            .
+            .
+            linha n = processo n
+        */
+
+        for (int i=0; i<matriz.length; i++){
+            for (int j = 0; j< matriz[0].length; j++){
+                Processo p = processos.get(i);
+                int chegada = p.getMomentoDeChegada();
+                
+                if (chegada > j || p.getRestingTime() == 0){ // O processo ainda nao chegou ou terminou de executar
+
+                    matriz[i][j] = 0; // nao faz nada
+
+                } else if (chegada <= j && p.getRestingTime()>0){ // o processo chegou e precisa executar
+                    /*
+                     verifica se naquele instante ha algum
+                     processo que esta esperando ou executando
+                     antes do processo atual
+                    */
+                    boolean livre = true;
+                    for (int linha=0; linha < i; linha++){  
+                        if (matriz[linha][j] != 0){
+                            livre = false;
+                        }
+                    }
+
+                    if (livre){
+                        matriz[i][j] = 1;
+                        p.diminuirRestingTime(); // executando
+
+                    } else {
+                        matriz[i][j] = 2; // esperando
+                    }
+                }
             }
         }
+
+        // ------------------------ANALISANDO A TABELA----------------------------------
+        for (int i=0; i<matriz.length; i++){
+            Processo p = processos.get(i);
+            for (int j = 0; j< matriz[0].length; j++){
+                if(matriz[i][j] == 2){ // esperando
+                    p.aumentarTempoDeEspera();
+                    p.aumentarTempoDeResposta();
+                }
+            }
+            p.setTurnaround();
+        }
+        
+        int somaTempoDeEspera = 0;
+        int somaTempoDeResposta = 0;
+        int somaTurnaround = 0;
+
+        for (processo : processos){
+            somaTempoDeEspera += processo.getTempoDeEspera();
+            somaTempoDeResposta += processo.getTempoDeResposta();
+            somaTurnaround += processo.getTurnaround();
+        }
+
+        tempoMedioDeEspera = somaTempoDeEspera / quantidadeDeProcessos;
+        tempoMedioDeResposta = somaTempoDeResposta / quantidadeDeProcessos;
+        turnaroundMedio = somaTurnaround / quantidadeDeProcessos;
     }
 
+    public int getTempoMedioDeEspera(){
+        return tempoMedioDeEspera;
+    }
 
+    public String getTempoMedioDeEspera(){
+        String tempo = String.valueOf(tempoMedioDeEspera);
+        return tempo;
+    }
 
+    public int getTempoMedioDeResposta(){
+        return tempoMedioDeResposta;
+    }
+
+    public String getTempoMedioDeResposta(){
+        String tempo = String.valueOf(tempoMedioDeResposta);
+        return tempo;
+    }
+
+    public int getTurnaroundMedio(){
+        return turnaroundMedio;
+    }
+    
+    public String getTurnaroundMedio(){
+        String tempo = String.valueOf(turnaroundMedio);
+        return tempo;
+    }
 }
